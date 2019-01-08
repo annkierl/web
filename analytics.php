287e24fb42392b4f677b0ee266783d38033f38c4
Michal [The Fox] Dyro <mdyro@atlassian.com>
2019-01-08 23:53:58 +0100

    1 version of analytics script

--- analytics.php
+++ analytics.php
@@ -1,19 +1,25 @@
 <?php
 
+// Connect to database
+$host = 'mysql.phost.biz';
+$db_username = 'anko_4_db';
+$db_password =  'wacmuf-Potzip-vapvi2';
+$db_name = 'anko';
+
+$table = 'tracks';
+
+$connection=mysqli_connect($host,$db_username,$db_password,$db_name);
 $request_method=$_SERVER["REQUEST_METHOD"];
 
 switch($request_method)
 {
     case 'GET':
         // Retrive data
-        if(!empty($_GET["product_id"]))
-        {
-            $product_id=intval($_GET["product_id"]);
-            get_data($product_id);
-        }
-        else
-        {
-            get_data();
+        if(!empty($_GET["user_id"])) {
+            $user_id = intval($_GET["user_id"]);
+            get_data($user_id);
+        } else {
+            get_unique_visitors();
         }
         break;
     case 'POST':
@@ -26,41 +32,78 @@ switch($request_method)
         break;
 }
 
-function get_data($product_id=0)
+function get_data($user_id=0)
 {
+    global $connection;
+    $query = "SELECT * FROM tracks WHERE user_id=".$user_id;
+
     $response=array();
 
-    header('Content-Typee: application/json');
-    header('Owner-Name: Anko');
+    $result=mysqli_query($connection, $query);
 
-    $response['UA'] = $_SERVER["HTTP_USER_AGENT"];
+    while($row=mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+        $response[]=$row;
+    }
+
+    header('Content-Type: application/json');
+
     echo json_encode($response);
 }
 
+
+
+function get_unique_visitors() {
+    global $connection;
+    $query = "SELECT DISTINCT user_id FROM tracks";
+
+    $response=array();
+
+    $result=mysqli_query($connection, $query);
+
+    while($row=mysqli_fetch_array($result, MYSQLI_NUM)) {
+        $response[]=$row;
+    }
+
+    header('Content-Type: application/json');
+
+    echo json_encode($response);
+}
+
+
+
 function insert_data()
 {
+    global $connection;
 
-//    global $connection;
-//    $product_name=$_POST["product_name"];
-//    $price=$_POST["price"];
-//    $quantity=$_POST["quantity"];
-//    $seller=$_POST["seller"];
-//    $query="INSERT INTO products SET product_name='{$product_name}', price={$price}, quantity={$quantity}, seller='{$seller}'";
-    if(false)
+    $data_str = file_get_contents('php://input');
+    $data = json_decode($data_str);
+
+    $user_id = $_GET["user_id"];
+
+    $data_sent_time = floor($data->data_sent_time / 1000 );
+
+    $mouse_move = json_encode($data->mouse_move);
+    $user_agent = $_SERVER["HTTP_USER_AGENT"];
+
+    $query="INSERT INTO tracks (user_id, data_sent_time, mouse_move, user_agent) VALUES ({$user_id},FROM_UNIXTIME({$data_sent_time}), '{$mouse_move}','{$user_agent}')";
+
+    if(mysqli_query($connection, $query))
     {
         $response=array(
             'status' => 1,
-            'status_message' =>'Product Added Successfully.'
+            'status_message' =>'Tracking Data Added Successfully.'
         );
     }
     else
     {
         $response=array(
             'status' => 0,
-            'status_message' =>'Product Addition Failed.'
+            'status_message' =>'Tracking Data Addition Failed.'
         );
     }
     header('Content-Type: application/json');
     echo json_encode($response);
 }
 
+// Close database connection
+mysqli_close($connection);
